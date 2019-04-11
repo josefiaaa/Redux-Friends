4fa2caa6c0618c975c3b5a34c067e1cd78522152
“Josefia <josefia.magagnoli@gmail.com>
2019-04-11 19:56:42 -0400

    Skeleton

---
+++ friends/src/action/index.js
@@ -0,0 +1,14 @@
+export { 
+    LOGIN_START,
+    LOGIN_SUCCESS, 
+    LOGIN_FAILURE, 
+    login 
+} from './login';
+
+
+export {
+  FETCH,
+  SUCCESS,
+  FAILURE,
+  getData
+} from './friendData';
